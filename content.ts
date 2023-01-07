d80fb31ab94e6415b6f513ebca58e713cec9644b
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-01-07 19:10:07 +0800

    feat: somewhat support arbitrary links

--- content.ts
+++ content.ts
@@ -1,9 +1,10 @@
 chrome.runtime.onMessage.addListener(async (message, sender) => {
   if (message !== 'feed_open') return false;
 
-  const elem = document.querySelector(
-    'link[rel="alternate"][type="application/rss+xml"]'
-  );
+  const elem =
+    document.querySelector(
+      'link[rel="alternate"][type="application/rss+xml"]'
+    ) ?? document.querySelector('a[href$="feed.xml"], a[href$="atom.xml"]');
   if (!elem) return false;
 
   const href = elem.getAttribute('href');
