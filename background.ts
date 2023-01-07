4e63196e31bef1ef69d3c0deb3f8169e325e4cc7
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-01-07 21:43:51 +0800

    refactor: use programmatic script injection and reduce permissions

--- background.ts
+++ background.ts
@@ -1,4 +1,30 @@
+const runOnTab = async () => {
+  const elem =
+    document.querySelector(
+      'link[rel="alternate"][type="application/rss+xml"]'
+    ) ??
+    document.querySelector(
+      'link[rel="alternate"][type="application/feed+json"]'
+    ) ??
+    document.querySelector('a[href$="feed.xml"], a[href$="atom.xml"]');
+
+  if (!elem) return false;
+
+  const href = elem.getAttribute('href');
+  if (!href) return false;
+
+  const url = href.startsWith('/') ? new URL(href, location.origin) : href;
+
+  window.open(`feed://${url}`);
+
+  return true;
+};
+
 chrome.action.onClicked.addListener(async (tab) => {
   if (!tab.id) return;
-  await chrome.tabs.sendMessage(tab.id, 'feed_open');
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: runOnTab,
+  });
 });
